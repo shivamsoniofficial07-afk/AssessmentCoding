2160b0a15d0adc8e6ed4ab784ee97552b744342d
shivamsoniofficial07-afk <shivamsoniofficial07@gmail.com>
2026-02-11 12:49:25 +0530

    initial commit

---
+++ src/main/java/com/web/assessement/AssessmentCodingApplication.java
@@ -0,0 +1,13 @@
+package com.web.assessement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentCodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/assessement/AssessmentCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.assessement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
